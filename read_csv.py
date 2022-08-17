6ad0aa64d0ea4ecbe80e8434fffe067d46dfeb2d
Sowjanya-Bommali <sowjanya.bommali@gmail.com>
2022-08-17 18:36:02 -0400

    Initial commit with kaggle datset

---
+++ read_csv.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+if __name__ == '__main__':
+    scSpark = SparkSession \
+        .builder \
+        .appName("reading csv") \
+        .getOrCreate()
+
+print(scSpark)
+
+data_file = './data*.csv'
+sdfData = scSpark.read.csv(data_file, header=True, sep=",").cache()
+print('Total Records = {}'.format(sdfData.count()))
+sdfData.show()

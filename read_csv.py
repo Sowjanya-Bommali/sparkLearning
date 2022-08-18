e8dc2f1d2cb2332674bb572a0bad7b9d642dbdb9
Sowjanya-Bommali <89036248+Sowjanya-Bommali@users.noreply.github.com>
2022-08-18 11:23:54 -0400

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
 print(scSpark)
 
-data_file = './data*.csv'
+data_file = './data/data*.csv'
 sdfData = scSpark.read.csv(data_file, header=True, sep=",").cache()
 print('Total Records = {}'.format(sdfData.count()))
 sdfData.show()
